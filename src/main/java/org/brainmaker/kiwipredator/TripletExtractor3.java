43f0b9d228e5271706f756a9b1c94f49ba004ced
Zekai <zekai@Brainmaker.(none)>
2013-02-17 02:25:24 -0500

    working on the rules defining

--- src/main/java/org/brainmaker/kiwipredator/TripletExtractor3.java
+++ src/main/java/org/brainmaker/kiwipredator/TripletExtractor3.java
@@ -26,58 +26,51 @@ import edu.stanford.nlp.trees.TreebankLanguagePack;
  * Not for the extraction task
  */
 public class TripletExtractor3 {
-	
-	
-	
-	
-	  public  void Parser(String filename) {
-		    // This option shows loading and sentence-segment and tokenizing
-		    // a file using DocumentPreprocessor
-		  
-		  LexicalizedParser lp = LexicalizedParser.loadModel();
-		    TreebankLanguagePack tlp = new PennTreebankLanguagePack();
-		    GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-		    // You could also create a tokenier here (as below) and pass it
-		    // to DocumentPreprocessor
-		    for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
-		    	if(sentence.size()>50)
-		    		continue;
-		      System.out.println(sentence.toString());
-		      Tree sen = lp.apply(sentence);
-		      sen.pennPrint();	
-		      
-		      sen = extractorFromTree(sen);
-		      
-		      if(sen!=null){
-		    	  Tree subgroup = ExtractSubGroup(sen.firstChild());
-		    	  if(subgroup!=null) subgroup.pennPrint();
-		      }
-		    }
-		  }
-	
-     
-	public static void main (String[] args) {
-		
-		// -f  folder
-		// -d  document
+
+	public void Parser(String filename) {
+		// This option shows loading and sentence-segment and tokenizing
+		// a file using DocumentPreprocessor
+
+		LexicalizedParser lp = LexicalizedParser.loadModel();
+		TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+		GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+		// You could also create a tokenier here (as below) and pass it
+		// to DocumentPreprocessor
+		for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
+			if (sentence.size() > 50)
+				continue;
+			System.out.println(sentence.toString());
+			Tree sen = lp.apply(sentence);
+			sen.pennPrint();
+
+			sen = extractorFromTree(sen);
+
+			if (sen != null) {
+				Tree subgroup = ExtractSubGroup(sen.firstChild());
+				if (subgroup != null)
+					subgroup.pennPrint();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// -f folder
+		// -d document
 		// -r raw
 		// -t tree
 		String type = args[0];
 		String filetype = args[1];
 		String path = args[2];
-		
-		if(args.length==3)
-		{
-			TripletExtractor3 te =  new TripletExtractor3();
-			if(type.equals("-r"))
-			{
+
+		if (args.length == 3) {
+			TripletExtractor3 te = new TripletExtractor3();
+			if (type.equals("-r")) {
 				te.Parser(path);
 			}
-		
-		}
-		else
-		{
-			
+
+		} else {
+
 			System.out.println("-r: indicating it is raw file");
 			System.out.println("-t: indicating it is tree file");
 			System.out.println("-f  path_to_folder");
@@ -86,13 +79,12 @@ public class TripletExtractor3 {
 			System.out.println("-r -d ./wsj-raw/00/wsj_0001");
 			System.out.println("-t -f ./Penn-trees/00");
 			System.out.println("-t -d ./Penn-trees/00/wsj_0001.mrg");
-			
+
 		}
-		
+
 	}
-	
-	
-	public Tree extractorFromTree(final Tree tree){
+
+	public Tree extractorFromTree(final Tree tree) {
 		RegularPrune rp = new RegularPrune();
 		RegularFilter rf = new RegularFilter();
 		rf.setRoot("S");
@@ -101,15 +93,14 @@ public class TripletExtractor3 {
 		Tree result = rp.prune(tree.firstChild(), rf);
 		return result;
 	}
-	
-	public Tree ExtractSubGroup(final Tree tree){
+
+	public Tree ExtractSubGroup(final Tree tree) {
 		RegularPrune rp = new RegularPrune();
 		RegularFilter rf = new RegularFilter();
 		rf.setRoot("NP");
 		rf.addChildRule("NNS");
 		Tree result = rp.prune(tree, rf);
-		if(result==null)
-		{
+		if (result == null) {
 			rf.reSetRule();
 			rf.setDepth1Num(1);
 			result = rp.prune(tree, rf);
@@ -118,4 +109,3 @@ public class TripletExtractor3 {
 	}
 
 }
-
